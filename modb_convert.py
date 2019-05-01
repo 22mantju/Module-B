1969627707feb0d40c7756336ba537dfbe1b2d88
22mantju <49483197+22mantju@users.noreply.github.com>
2019-05-01 12:19:18 -0400

    modb_convert.py
    Makes a list of Celsius and Fahrenheit in a list

---
+++ modb_convert.py
@@ -0,0 +1,17 @@
+# File: convert.py
+# A program to convert Celsius temps to Fahrenheit
+# Modified 05/01/19
+
+def main():
+  print("Convert Celsius to Fahrenheit")
+  print("Celsius temperature\tFahrenheit Equivalent")
+  for currentCelsiusTemperature in range(0,101,10):
+    fahrenheitTemperatureEquvalent = ( 9 / 5 ) *currentCelsiusTemperature + 32
+    print( currentCelsiusTemperature,"\t\t\t", \
+      format( fahrenheitTemperatureEqucalent, ".1f") )
+  for i in range(5):
+    celsius = eval(input("What is the Celsius temperature? "))
+    fahrenheit = 9 / 5 * celsius + 32
+    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
+  input("Press the <Enter> to quit.")
+main()
