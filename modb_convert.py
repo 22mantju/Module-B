3ae9e700bc50b381767de5b960efd8c25bd687e1
22mantju <49483197+22mantju@users.noreply.github.com>
2019-05-13 13:33:43 -0400

    Add more code on "Line 27"

--- modb_convert.py
+++ modb_convert.py
@@ -3,16 +3,39 @@
 # Modified 05/01/19
 # By: Justin Mantis
 
+def ctf():
+    for i in range(5):
+        Celsius = eval(input("What is the Celsius temperature? "))
+        Fahrenheit = 9 / 5 * Celsius + 32
+        print("The temperature is", Fahrenheit, "degrees Fahrenheit.")
+        print(" ")
+    
+
+
+def ftc():
+    for i in range(5):
+        Fahrenheit = eval(input("What is the fahrenheit temperature?"))
+        Celsius = (Fahrenheit-32)*5/9 
+        print("The temperature is",Celsius,"degrees celsius.")
+        print(" ")
+
 def main():
-  print("Convert Celsius to Fahrenheit")
-  print("Celsius temperature\tFahrenheit Equivalent")
-  for currentCelsiusTemperature in range(0,101,10):
-    fahrenheitTemperatureEquvalent = ( 9 / 5 ) *currentCelsiusTemperature + 32
-    print( currentCelsiusTemperature,"\t\t\t", \
-      format( fahrenheitTemperatureEqucalent, ".1f") )
-  for i in range(5):
-    celsius = eval(input("What is the Celsius temperature? "))
-    fahrenheit = 9 / 5 * celsius + 32
-    print("The temperature is", fahrenheit, "degrees Fahrenheit.")
-  input("Press the <Enter> to quit.")
+    print("Convert Celsius to Fahrenheit")
+    print("Celsius temperature \t Fahrenheit Equivalent")
+    for currentCelsiusTemperature in range(0,101,10):
+        fahrenheitTemperatureEquvalent = ( 9 / 5 ) *currentCelsiusTemperature + 32
+        print( currentCelsiusTemperature,"°C","\t\t\t",fahrenheitTemperatureEquvalent,"°F")
+    uc = input("Do you want to convert C-->F(c) or F-->C(f) or quit(q):")
+    while uc !='q':
+        if uc == 'c':
+            ctf()
+            uc = input("Do you want to convert C-->(c) ro F-->(f) or quit(q): ")
+        elif uc == 'f':
+            ftc()
+            uc = input("Do you want to convert C--F(c) or F-->(f) or quit(q): ")
+        elif uc !='c' and uc !='f':
+            print("Please put a VALID INPUT!")
+            uc = input("Do you want to convert C-->F(c) or F-->C(f) or quit(q): ")
+    print("Now exiting...")
+
 main()
